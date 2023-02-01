351ab13ef8aae5c28af5b2ccb4f7cfd3094c86e2
juma <jumaaj@protonmail.com>
2023-02-01 11:19:17 +0300

    responsive layout

---
+++ script.js
@@ -0,0 +1,11 @@
+const menuOpenEl = document.querySelector(".menu-icon");
+const modalCloseEl = document.querySelector(".modal-close");
+const modalEl = document.querySelector(".modal");
+
+menuOpenEl.addEventListener("click", () => {
+  modalEl.classList.remove("modal-hidden");
+});
+
+modalCloseEl.addEventListener("click", () => {
+  modalEl.classList.add("modal-hidden");
+});
