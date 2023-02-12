e7ddf4a2403548c4b0b9d46b795f3510b71161a4
juma <jumaaj@protonmail.com>
2023-02-12 18:00:23 +0300

    site rework

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const menuOpenEl = document.querySelector(".menu-icon");
-const modalCloseEl = document.querySelector(".modal-close");
-const modalEl = document.querySelector(".modal");
+// const menuOpenEl = document.querySelector(".menu-icon");
+// const modalCloseEl = document.querySelector(".modal-close");
+// const modalEl = document.querySelector(".modal");
 
-menuOpenEl.addEventListener("click", () => {
-  modalEl.classList.remove("modal-hidden");
-});
+// menuOpenEl.addEventListener("click", () => {
+//   modalEl.classList.remove("modal-hidden");
+// });
 
-modalCloseEl.addEventListener("click", () => {
-  modalEl.classList.add("modal-hidden");
-});
+// modalCloseEl.addEventListener("click", () => {
+//   modalEl.classList.add("modal-hidden");
+// });
